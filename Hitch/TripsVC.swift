15b2e8cce598db105d52a5ca3267ea621a260c38
Andrew Roach <roach1750@gmail.com>
2016-10-27 22:24:19 -0400

    added TVC

---
+++ Hitch/TripsVC.swift
@@ -0,0 +1,56 @@
+//
+//  TripsVC.swift
+//  Hitch
+//
+//  Created by Andrew Roach on 10/27/16.
+//  Copyright © 2016 Andrew Roach. All rights reserved.
+//
+
+import UIKit
+
+class TripsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tripCell", for: indexPath)
+        
+        // Configure the cell...
+        
+        return cell
+    }
+
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+
+}
